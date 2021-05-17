893aa368fe6b30aed2a80ee6b7d9d5b763496992
aika <aiganysha@gmail.com>
2021-05-17 14:23:12 -0500

    text added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,8 @@ const Header = () => {
             <nav class="navbar navbar-light bg-color ">
                 <span class="navbar-brand mb-0 h1">Shoppies Awards</span>
             </nav>
+            <h5 className="text">Browse and save 5 of your favourite movies for the Shoppies Nominations!</h5>
+
         </>
     )
 }
